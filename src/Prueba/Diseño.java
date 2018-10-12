e3c7c4d3de3730b2e5ba1490cdb88a4a1985529a
Gustavo <Gustavo@10.5.28.113>
2018-10-12 18:19:57 -0500

    ujyhgfvdcsxa

---
+++ src/Prueba/Diseño.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Diseño {
+    public void saludo(){
+        System.out.println("Hola Mundo");
+    }
+}
